3727c1b3bea3fef0b1c018ba37b3694044a03043
unknown <dhoundiyalswati11@gmail.com>
2019-06-07 14:50:33 +0530

    Fifth commit

--- src/main/java/in/ac/sharda/pizz_app/controller/CartController.java
+++ src/main/java/in/ac/sharda/pizz_app/controller/CartController.java
@@ -1,10 +1,24 @@
 package in.ac.sharda.pizz_app.controller;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class CartController {
+import in.ac.sharda.pizz_app.domain.Cart;
+import in.ac.sharda.pizz_app.domain.Product;
+//4.) CREATED A REST CONTROLLER 
 
+@RestController
+
+public class CartController {
+	// 5.) ADDED A LIST OF CART HAVING PRODUCT
+	private List<Product> products = new ArrayList<>();
+
+	public void addProduct(Product product) {
+		this.products.add(product);
+	}
 	
-	// 4.) CREATED A REST CONTROLLER  
+	
+	  
 }
