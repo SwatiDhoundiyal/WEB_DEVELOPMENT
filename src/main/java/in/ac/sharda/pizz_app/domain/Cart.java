a68fcc7bdc0abe5d58aca5db9fc9a30f85f3df7f
unknown <dhoundiyalswati11@gmail.com>
2019-06-07 14:59:24 +0530

    eighth commit

--- src/main/java/in/ac/sharda/pizz_app/domain/Cart.java
+++ src/main/java/in/ac/sharda/pizz_app/domain/Cart.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
+	private int id;     //*** 8.) ADDED A UNIQUE ITEM IN THE CART
 	// IMPORTED A LIST NAMED AS PRODUCT
 	// ...Added a new Array List
 	private List<Product> products = new ArrayList<>();
